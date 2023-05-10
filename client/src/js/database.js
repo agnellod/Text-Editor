07fa70a04cb848a407065ffd565ab14dff5a0943
Dylan <agnello.dylan95@gmail.com>
2023-05-10 11:09:06 -0600

    fixing last of the bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('PUT to the database', id);
+  console.error('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
 
@@ -39,11 +39,11 @@ const tx = jateDb.transaction('jate', 'readonly');
 
 const store = tx.objectStore('jate');
 
-const request = store.getAll();
+const request = store.get(1);
 
 const result = await request;
 console.log('result.value', result);
-return result;
+return result?.value;
 }
 
 initdb();
